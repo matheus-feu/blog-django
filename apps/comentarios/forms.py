aafdfd39cdda8b28007aaa951281e1a26c316104
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-10 17:08:05 -0300

    feat: add recaptcha em form commentários

--- apps/comentarios/forms.py
+++ apps/comentarios/forms.py
@@ -1,11 +1,35 @@
-from django.forms import ModelForm
+import requests
 
+from django.forms import ModelForm
 from .models import Comments
 
 
 class CommentForm(ModelForm):
     """Formulário de comentários"""
+
     def clean(self):
+        """Validação do formulário, recaptcha e campos obrigatórios"""
+
+        # Instanciando o recaptcha
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        # Verificando se o recaptcha foi marcado
+        recaptcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LcYfu4kAAAAAL2aSJl1vNd06bm0qsmpj54vTJMW',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+
+        # Se não foi marcado, adiciona um erro
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comment',
+                'Você precisa marcar o campo "Não sou um robô"'
+            )
+
         cleaned_data = super(CommentForm, self).clean()
         name_com = cleaned_data.get('name_com')
         email_com = cleaned_data.get('email_com')
